a04e2d2b050797105093b9eaa1749534e08c7060
Moirted <33633669+Moirted@users.noreply.github.com>
2021-03-03 16:34:11 +0300

    Add files via upload

---
+++ data/1000 - 7.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var let_me_die := 1000;
+  while true do
+  begin
+    WriteLnFormat('{0} - 7 = {1}', let_me_die, let_me_die - 7);
+    System.Threading.Thread.Sleep(100);
+    let_me_die := let_me_die - 7;
+    if let_me_die = 6 then
+      let_me_die := 1000;
+  end;
+end.
