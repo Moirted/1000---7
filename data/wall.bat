b1e336cec2042eb619c84ec107d92be682ec8a69
Moirted <moirted@gmail.com>
2021-03-03 16:22:01 +0300

    init 2

--- data/wall.bat
+++
@@ -1,6 +0,0 @@
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v wallpaper /t REG_SZ /d "%CD%\walls.bmp" /f
-reg add "HKEY_CURRENT_USER\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-pause
-exit /b
